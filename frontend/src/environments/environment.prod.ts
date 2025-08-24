3be5862e2eca93e3726ba2450d852c5a8ecf8357
mostafa172 <mostafa17_2@live.com>
2025-08-25 00:02:27 +0300

    Push prod url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api', // Replace with your production API URL
+  apiUrl: 'https://premier-league-predictions-kz0m.onrender.com', // Replace with your production API URL
   // Add any other production-specific settings here
 };
