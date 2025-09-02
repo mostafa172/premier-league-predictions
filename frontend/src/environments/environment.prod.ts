ab9a65403ffe2d174cdadb4965f03740b1f044e3
mostafa172 <mostafa17_2@live.com>
2025-09-02 18:29:26 +0300

    Fix prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://premier-league-predictions-kz0m.onrender.com/api', // Replace with your production API URL
+  apiUrl: "https://premier-league-predictions-kz0m.onrender.com/api", // Replace with your production API URL
   // Add any other production-specific settings here
 };
+
+console.log("🔴 PROD Environment loaded:", environment);
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: "http://localhost:3000/api",
   // Add other environment-specific settings here
 };
+
+console.log("🟢 DEV Environment loaded:", environment);
